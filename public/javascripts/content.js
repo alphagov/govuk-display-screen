afb41b1a88fb54afff25573d340dfaf88274bb32
Rosa Fox <rosa.fox@digital.cabinet-office.gov.uk>
2022-10-26 17:32:49 +0100

    Display data from /popular-content endpoint on dashboard
    - Update title to reflect that trending content is being replaced by most popular content
    - Improving the styling with the design system will happen as a follow up task
    
    Co-authored-by: Graham Lewis <graham.lewis@digital.cabinet-office.gov.uk>
    Co-authored-by: Alex Newton <alex.newton@digital.cabinet-office.gov.uk>

--- public/javascripts/content.js
+++ public/javascripts/content.js
@@ -9,25 +9,16 @@
     $el: false,
 
     endpoint: function(){
-      return "https://www.performance.service.gov.uk/data/govuk/trending?limit=15&sort_by=percent_change:descending";
+      return "/popular-content";
     },
     parseResponse: function(data){
-      var i, _i, title;
-
       content.pages = [];
-      for(i=0,_i=data.data.length; i<_i; i++){
-        if(data.data[i].pageTitle.indexOf(' - ') > -1){
-          title = data.data[i].pageTitle.split(' - ').slice(0,-1).join(' - ');
-        } else {
-          title = data.data[i].pageTitle.split(' | ').slice(0,-1).join(' - ');
-        }
-        content.pages.push({
-          title: title,
-          displayHits: root.matrix.numberWithCommas(data.data[i].week2),
-          percentageUp: root.matrix.numberWithCommas(Math.round(data.data[i].percent_change)) + "%"
+      for (let i = 0; i < data.length; i++) {
+        content.pages.push ({
+          title: data[i]["page_title"],
+          displayHits: data[i]["page_views"],
         });
       }
-
       content.displayResults();
     },
     displayResults: function(){
